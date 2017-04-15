52c63884a45f1ced9bacefbcd1c7a4da45b26e0d
SaberSnail <andrew@batishko.net>
2017-04-15 08:55:24 -0700

    Add ObjectImpl

---
+++ GoldenAnvil.Utility/ObjectImpl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GoldenAnvil.Utility
+{
+	public static class ObjectImpl
+	{
+		public static bool OperatorEquality<T>(T left, T right)
+			where T : class, IEquatable<T>
+		{
+			if (ReferenceEquals(left, right))
+				return true;
+			if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
+				return false;
+			return left.Equals(right);
+		}
+
+		public static bool OperatorInequality<T>(T left, T right)
+			where T : class, IEquatable<T>
+		{
+			if (ReferenceEquals(left, right))
+				return false;
+			if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
+				return true;
+			return !left.Equals(right);
+		}
+	}
+}
