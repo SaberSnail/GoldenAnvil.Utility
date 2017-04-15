1a3b5ec4fe4b243fece4b6e520dd466b2b827f90
SaberSnail <andrew@batishko.net>
2017-04-15 08:54:49 -0700

    Add DictionaryUtility

---
+++ GoldenAnvil.Utility/DictionaryUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GoldenAnvil.Utility
+{
+	public static class DictionaryUtility
+	{
+		public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+		{
+			TValue value;
+			dictionary.TryGetValue(key, out value);
+			return value;
+		}
+
+		public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+		{
+			TValue value;
+			return dictionary.TryGetValue(key, out value) ? value : defaultValue;
+		}
+
+		public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> defaultCreator)
+		{
+			TValue value;
+			return dictionary.TryGetValue(key, out value) ? value : defaultCreator();
+		}
+	}
+}
