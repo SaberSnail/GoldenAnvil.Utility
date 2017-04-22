de666bfcf1ce24316bf4dfcf6907301bb9aab142
SaberSnail <andrew@batishko.net>
2017-04-22 10:46:58 -0700

    Add support for timing log messages

---
+++ GoldenAnvil.Utility/Logging/LogSourceUtility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GoldenAnvil.Utility.Logging
+{
+	public static class LogSourceUtility
+	{
+		public static Scope TimedInfo(this ILogSource logger, string message)
+		{
+			return CreateTimedLogScope(logger.Info, message);
+		}
+
+		public static Scope TimedWarn(this ILogSource logger, string message)
+		{
+			return CreateTimedLogScope(logger.Warn, message);
+		}
+
+		public static Scope TimedError(this ILogSource logger, string message)
+		{
+			return CreateTimedLogScope(logger.Error, message);
+		}
+
+		private static Scope CreateTimedLogScope(Action<string> doLogMessage, string message)
+		{
+			Stopwatch stopwatch = Stopwatch.StartNew();
+			doLogMessage($"(start) {message}");
+			return Scope.Create(() =>
+			{
+				TimeSpan elapsed = stopwatch.Elapsed;
+				doLogMessage($"({elapsed}) {message}");
+			});
+		}
+	}
+}
