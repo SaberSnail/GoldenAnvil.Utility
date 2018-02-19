228625ce3affb90ec4d23aa9959baadca11fa0e3
SaberSnail <andrew@batishko.net>
2018-02-19 12:42:59 -0800

    Add DictionaryUtility.GetOrAddValue

--- GoldenAnvil.Utility/DictionaryUtility.cs
+++ GoldenAnvil.Utility/DictionaryUtility.cs
@@ -7,21 +7,41 @@ namespace GoldenAnvil.Utility
 	{
 		public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
 		{
-			TValue value;
-			dictionary.TryGetValue(key, out value);
+			dictionary.TryGetValue(key, out TValue value);
 			return value;
 		}
 
 		public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
 		{
-			TValue value;
-			return dictionary.TryGetValue(key, out value) ? value : defaultValue;
+			return dictionary.TryGetValue(key, out TValue value) ? value : defaultValue;
 		}
 
 		public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> defaultCreator)
 		{
-			TValue value;
-			return dictionary.TryGetValue(key, out value) ? value : defaultCreator();
+			return dictionary.TryGetValue(key, out TValue value) ? value : defaultCreator();
+		}
+
+		public static TValue GetOrAddValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+		{
+			if (!dictionary.ContainsKey(key))
+			{
+				dictionary[key] = value;
+				return value;
+			}
+
+			return dictionary[key];
+		}
+
+		public static TValue GetOrAddValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> valueCreator)
+		{
+			if (!dictionary.ContainsKey(key))
+			{
+				var value = valueCreator();
+				dictionary[key] = value;
+				return value;
+			}
+
+			return dictionary[key];
 		}
 	}
 }
