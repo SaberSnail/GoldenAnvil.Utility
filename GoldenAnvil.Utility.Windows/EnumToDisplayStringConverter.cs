2fa6a07098321cad61b41559132ed6ce0219bbb7
SaberSnail <andrew@batishko.net>
2018-02-19 18:51:15 -0800

    Add utilities for getting localized enum values

---
+++ GoldenAnvil.Utility.Windows/EnumToDisplayStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Resources;
+using System.Windows.Data;
+
+namespace GoldenAnvil.Utility.Windows
+{
+	public sealed class EnumToDisplayStringConverter : IValueConverter
+	{
+		public static readonly EnumToDisplayStringConverter Instance = new EnumToDisplayStringConverter();
+
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (!(parameter is ResourceManager resources))
+				throw new ArgumentException($"{nameof(parameter)} must be a ResourceManager.");
+
+			var method = typeof(ResourceManagerUtility).GetMethod("EnumToDisplayString");
+			var generic = method.MakeGenericMethod(value.GetType());
+			return generic.Invoke(null, new [] { resources, value });
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ GoldenAnvil.Utility/ResourceManagerUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Resources;
+
+namespace GoldenAnvil.Utility
+{
+	public static class ResourceManagerUtility
+	{
+		public static string EnumToDisplayString<T>(this ResourceManager resources, T enumValue) where T : struct
+		{
+			if (!typeof(T).IsEnum)
+				throw new ArgumentException(@"T must be an enumerated type.");
+
+			var key = $"{typeof(T).Name}_{enumValue.ToString()}";
+			return resources.GetString(key) ?? enumValue.ToString();
+		}
+	}
+}
