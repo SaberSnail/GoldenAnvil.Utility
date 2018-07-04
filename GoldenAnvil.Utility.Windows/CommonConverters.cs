761cedb0f4a2b89e82a9b8e8e3b982aa736fd285
SaberSnail <andrew@batishko.net>
2018-07-04 10:00:32 -0700

    Add CommonConverters

---
+++ GoldenAnvil.Utility.Windows/CommonConverters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace GoldenAnvil.Utility.Windows
+{
+	public static class CommonConverters
+	{
+		public static readonly IValueConverter BooleanNot = new BooleanNotConverter();
+
+		private sealed class BooleanNotConverter : IValueConverter
+		{
+			public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+			{
+				if (targetType != typeof(bool))
+					throw new InvalidOperationException(@"The target must be a boolean.");
+
+				return !((bool) value);
+			}
+
+			public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+}
