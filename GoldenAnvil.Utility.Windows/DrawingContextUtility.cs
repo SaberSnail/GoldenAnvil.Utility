47198855ba1eeed389302452cdd1aacf3248ff54
SaberSnail <andrew@batishko.net>
2018-07-08 18:04:01 -0700

    Add DrawingContextUtility

---
+++ GoldenAnvil.Utility.Windows/DrawingContextUtility.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Media;
+
+namespace GoldenAnvil.Utility.Windows
+{
+	public static class DrawingContextUtility
+	{
+		public static Scope ScopedTransform(this DrawingContext context, Transform transform)
+		{
+			context.PushTransform(transform);
+			return Scope.Create(context.Pop);
+		}
+
+		public static Scope ScopedClip(this DrawingContext context, Geometry clip)
+		{
+			context.PushClip(clip);
+			return Scope.Create(context.Pop);
+		}
+	}
+}
