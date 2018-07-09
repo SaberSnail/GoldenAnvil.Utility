6f9591e665da8cc43ed29343c539ce0d5ecfd1e1
SaberSnail <andrew@batishko.net>
2018-07-08 18:03:49 -0700

    Add default values to DependencyPropertyUtility

--- GoldenAnvil.Utility.Windows/DependencyPropertyUtility.cs
+++ GoldenAnvil.Utility.Windows/DependencyPropertyUtility.cs
@@ -6,19 +6,19 @@ namespace GoldenAnvil.Utility.Windows
 {
 	public static class DependencyPropertyUtility<T>
 	{
-		public static DependencyProperty Register<TValue>(Expression<Func<T, TValue>> selector)
+		public static DependencyProperty Register<TValue>(Expression<Func<T, TValue>> selector, TValue defaultValue = default(TValue))
 		{
-			return DependencyProperty.Register(ReflectionUtility<T>.GetMemberName(selector), ReflectionUtility<T>.GetMemberType(selector), typeof(T));
+			return DependencyProperty.Register(ReflectionUtility<T>.GetMemberName(selector), ReflectionUtility<T>.GetMemberType(selector), typeof(T), new PropertyMetadata(defaultValue));
 		}
 
-		public static DependencyProperty Register<TValue>(Expression<Func<T, TValue>> selector, PropertyChangedCallback changedCallback)
+		public static DependencyProperty Register<TValue>(Expression<Func<T, TValue>> selector, PropertyChangedCallback changedCallback, TValue defaultValue = default(TValue))
 		{
-			return DependencyProperty.Register(ReflectionUtility<T>.GetMemberName(selector), ReflectionUtility<T>.GetMemberType(selector), typeof(T), new PropertyMetadata(changedCallback));
+			return DependencyProperty.Register(ReflectionUtility<T>.GetMemberName(selector), ReflectionUtility<T>.GetMemberType(selector), typeof(T), new PropertyMetadata(defaultValue, changedCallback));
 		}
 
-		public static DependencyProperty Register<TValue>(Expression<Func<T, TValue>> selector, PropertyChangedCallback changedCallback, FrameworkPropertyMetadataOptions flags)
+		public static DependencyProperty Register<TValue>(Expression<Func<T, TValue>> selector, PropertyChangedCallback changedCallback, FrameworkPropertyMetadataOptions flags, TValue defaultValue = default(TValue))
 		{
-			return DependencyProperty.Register(ReflectionUtility<T>.GetMemberName(selector), ReflectionUtility<T>.GetMemberType(selector), typeof(T), new FrameworkPropertyMetadata(default(T), flags, changedCallback));
+			return DependencyProperty.Register(ReflectionUtility<T>.GetMemberName(selector), ReflectionUtility<T>.GetMemberType(selector), typeof(T), new FrameworkPropertyMetadata(defaultValue, flags, changedCallback));
 		}
 
 		public static DependencyPropertyKey RegisterReadOnly<TValue>(Expression<Func<T, TValue>> selector)
