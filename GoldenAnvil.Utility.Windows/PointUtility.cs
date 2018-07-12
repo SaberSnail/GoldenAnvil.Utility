b355535f705e55f85247c5a3b445e389c87d1ed4
SaberSnail <andrew@batishko.net>
2018-07-12 05:01:44 -0700

    Add PointUtility

---
+++ GoldenAnvil.Utility.Windows/PointUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+
+namespace GoldenAnvil.Utility.Windows
+{
+	public static class PointUtility
+	{
+		public static double DistanceTo(this Point point, Point target)
+		{
+			var dx = point.X - target.X;
+			var dy = point.Y - target.Y;
+			return Math.Sqrt((dx * dx) + (dy * dy));
+		}
+	}
+}
