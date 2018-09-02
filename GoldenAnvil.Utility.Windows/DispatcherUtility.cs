ba69bbe50d6f2c24c94b868d1290d19564ebf9ab
SaberSnail <andrew@batishko.net>
2018-09-02 08:18:08 -0700

    Add DispatcherUtility

---
+++ GoldenAnvil.Utility.Windows/DispatcherUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Windows.Threading;
+
+namespace GoldenAnvil.Utility.Windows
+{
+	public static class DispatcherUtility
+	{
+		public static void VerifyCurrent(this Dispatcher dispatcher, [CallerMemberName] string callerName = "")
+		{
+			if (dispatcher != Dispatcher.CurrentDispatcher)
+				throw new InvalidOperationException($"{callerName} must be running on the dispatcher thread.");
+		}
+
+		public static void VerifyNotCurrent(this Dispatcher dispatcher, [CallerMemberName] string callerName = "")
+		{
+			if (dispatcher == Dispatcher.CurrentDispatcher)
+				throw new InvalidOperationException($"{callerName} must not be running on the dispatcher thread.");
+		}
+	}
+}
