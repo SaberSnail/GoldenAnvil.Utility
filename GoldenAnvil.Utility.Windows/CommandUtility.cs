1315eb69c423ca36a09c9aa03d2d9ff8566cb8c4
SaberSnail <andrew@batishko.net>
2018-10-07 08:28:56 -0700

    Add RoutedCommandToCommandBindingConverter

---
+++ GoldenAnvil.Utility.Windows/CommandUtility.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GoldenAnvil.Utility.Windows
+{
+	public static class CommandUtility
+	{
+		public static readonly DependencyProperty MergedCommandBindingsProperty = DependencyProperty.RegisterAttached("MergedCommandBindings", typeof(IReadOnlyCollection<CommandBinding>), typeof(CommandUtility), new PropertyMetadata(null, OnMergedCommandBindingsChanged));
+
+		public static string GetMergedCommandBindings(DependencyObject d)
+		{
+			return (string) d.GetValue(MergedCommandBindingsProperty);
+		}
+
+		public static void SetMergedCommandBindings(DependencyObject d, string value)
+		{
+			d.SetValue(MergedCommandBindingsProperty, value);
+		}
+
+		private static void OnMergedCommandBindingsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			if (d == null)
+				return;
+
+			var commandBindings = GetCommandBindings(d);
+			if (commandBindings is null)
+				throw new InvalidOperationException($"{MergedCommandBindingsProperty.Name} is attached to a DependencyObject that does not have CommandBindings.");
+
+			if (e.OldValue != null)
+			{
+				foreach (var commandBinding in (IReadOnlyCollection<CommandBinding>) e.OldValue)
+					commandBindings.Remove(commandBinding);
+			}
+
+			if (e.NewValue != null)
+			{
+				foreach (var commandBinding in (IReadOnlyCollection<CommandBinding>) e.NewValue)
+					commandBindings.Add(commandBinding);
+			}
+
+			CommandManager.InvalidateRequerySuggested();
+		}
+
+		private static CommandBindingCollection GetCommandBindings(DependencyObject d)
+		{
+			if (d is UIElement uiElement)
+				return uiElement.CommandBindings;
+			if (d is ContentElement contentElement)
+				return contentElement.CommandBindings;
+			if (d is UIElement3D uiElement3D)
+				return uiElement3D.CommandBindings;
+
+			return null;
+		}
+	}
+}
---
+++ GoldenAnvil.Utility.Windows/RoutedCommandToCommandBindingConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace GoldenAnvil.Utility.Windows
+{
+	public sealed class RoutedCommandToCommandBindingConverter : IMultiValueConverter
+	{
+		public static readonly RoutedCommandToCommandBindingConverter Instance = new RoutedCommandToCommandBindingConverter();
+
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			List<CommandBinding> commandBindings = new List<CommandBinding>();
+			for (var index = 0; index < values.Length; index++)
+			{
+				var routedCommand = values[index] as RoutedCommand;
+				index++;
+				if (index != values.Length)
+				{
+					var command = values[index] as ICommand;
+					if (routedCommand != null && command != null)
+					{
+						commandBindings.Add(new CommandBinding(
+							routedCommand,
+							(sender, args) => command.Execute(args.Parameter),
+							(sender, args) => args.CanExecute = command.CanExecute(args.Parameter)));
+					}
+				}
+			}
+
+			return commandBindings;
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
