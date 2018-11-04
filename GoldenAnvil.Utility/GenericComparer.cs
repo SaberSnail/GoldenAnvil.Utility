675f465fae3d6a63737754dd6d836befc4331630
SaberSnail <andrew@batishko.net>
2018-11-04 13:55:27 -0800

    Add GenericComparer

---
+++ GoldenAnvil.Utility/GenericComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GoldenAnvil.Utility
+{
+	public sealed class GenericComparer<T> : IComparer<T>
+	{
+		public GenericComparer(Func<T, T, int> compareFunc)
+		{
+			if (compareFunc == null)
+				throw new ArgumentNullException(nameof(compareFunc));
+
+			m_compareFunc = compareFunc;
+		}
+
+		public int Compare(T left, T right)
+		{
+			return m_compareFunc(left, right);
+		}
+
+		readonly Func<T, T, int> m_compareFunc;
+	}
+}
