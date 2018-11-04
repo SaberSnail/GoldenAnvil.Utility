a54a8737a882b76d6ac13114f5d3e5eed1f1c0d1
SaberSnail <andrew@batishko.net>
2018-11-04 13:55:16 -0800

    Add Join extension for strings

--- GoldenAnvil.Utility/StringUtility.cs
+++ GoldenAnvil.Utility/StringUtility.cs
@@ -1,4 +1,6 @@
-﻿using System.Globalization;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
 
 namespace GoldenAnvil.Utility
 {
@@ -7,6 +9,19 @@ namespace GoldenAnvil.Utility
 		public static string FormatInvariant(this string format, params object[] args)
 		{
 			return string.Format(CultureInfo.InvariantCulture, format, args);
+		}
+
+		public static string Join(this IEnumerable<string> parts, string joinText)
+		{
+			var builder = new StringBuilder();
+			foreach (var part in parts)
+			{
+				if (builder.Length != 0)
+					builder.Append(joinText);
+				builder.Append(part);
+			}
+
+			return builder.ToString();
 		}
 	}
 }
