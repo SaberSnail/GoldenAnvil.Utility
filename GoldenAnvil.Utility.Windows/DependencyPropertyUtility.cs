fcaf141f2b0a68c3fb20f452f1814ce54801700e
SaberSnail <andrew@batishko.net>
2019-06-02 19:23:43 -0700

    Add registration of attached properties

--- GoldenAnvil.Utility.Windows/DependencyPropertyUtility.cs
+++ GoldenAnvil.Utility.Windows/DependencyPropertyUtility.cs
@@ -8,27 +8,42 @@ namespace GoldenAnvil.Utility.Windows
 	{
 		public static DependencyProperty Register<TValue>(Expression<Func<T, TValue>> selector, TValue defaultValue = default(TValue))
 		{
-			return DependencyProperty.Register(ReflectionUtility<T>.GetMemberName(selector), ReflectionUtility<T>.GetMemberType(selector), typeof(T), new PropertyMetadata(defaultValue));
+			return DependencyProperty.Register(ReflectionUtility<T>.GetMemberName(selector), typeof(TValue), typeof(T), new PropertyMetadata(defaultValue));
 		}
 
 		public static DependencyProperty Register<TValue>(Expression<Func<T, TValue>> selector, PropertyChangedCallback changedCallback, TValue defaultValue = default(TValue))
 		{
-			return DependencyProperty.Register(ReflectionUtility<T>.GetMemberName(selector), ReflectionUtility<T>.GetMemberType(selector), typeof(T), new PropertyMetadata(defaultValue, changedCallback));
+			return DependencyProperty.Register(ReflectionUtility<T>.GetMemberName(selector), typeof(TValue), typeof(T), new PropertyMetadata(defaultValue, changedCallback));
 		}
 
 		public static DependencyProperty Register<TValue>(Expression<Func<T, TValue>> selector, PropertyChangedCallback changedCallback, FrameworkPropertyMetadataOptions flags, TValue defaultValue = default(TValue))
 		{
-			return DependencyProperty.Register(ReflectionUtility<T>.GetMemberName(selector), ReflectionUtility<T>.GetMemberType(selector), typeof(T), new FrameworkPropertyMetadata(defaultValue, flags, changedCallback));
+			return DependencyProperty.Register(ReflectionUtility<T>.GetMemberName(selector), typeof(TValue), typeof(T), new FrameworkPropertyMetadata(defaultValue, flags, changedCallback));
 		}
 
 		public static DependencyPropertyKey RegisterReadOnly<TValue>(Expression<Func<T, TValue>> selector)
 		{
-			return DependencyProperty.RegisterReadOnly(ReflectionUtility<T>.GetMemberName(selector), ReflectionUtility<T>.GetMemberType(selector), typeof(T), null);
+			return DependencyProperty.RegisterReadOnly(ReflectionUtility<T>.GetMemberName(selector), typeof(TValue), typeof(T), null);
 		}
 
 		public static DependencyPropertyKey RegisterReadOnly<TValue>(Expression<Func<T, TValue>> selector, PropertyChangedCallback changedCallback)
 		{
-			return DependencyProperty.RegisterReadOnly(ReflectionUtility<T>.GetMemberName(selector), ReflectionUtility<T>.GetMemberType(selector), typeof(T), new PropertyMetadata(changedCallback));
+			return DependencyProperty.RegisterReadOnly(ReflectionUtility<T>.GetMemberName(selector), typeof(TValue), typeof(T), new PropertyMetadata(changedCallback));
+		}
+
+		public static DependencyProperty RegisterAttached<TValue>(string name)
+		{
+			return DependencyProperty.RegisterAttached(name, typeof(TValue), typeof(T));
+		}
+
+		public static DependencyProperty RegisterAttached<TValue>(string name, PropertyChangedCallback changedCallback)
+		{
+			return DependencyProperty.RegisterAttached(name, typeof(TValue), typeof(T), new PropertyMetadata(changedCallback));
+		}
+
+		public static DependencyProperty RegisterAttached<TValue>(string name, PropertyChangedCallback changedCallback, FrameworkPropertyMetadataOptions flags)
+		{
+			return DependencyProperty.RegisterAttached(name, typeof(TValue), typeof(T), new FrameworkPropertyMetadata(default(TValue) , flags, changedCallback));
 		}
 	}
 }
