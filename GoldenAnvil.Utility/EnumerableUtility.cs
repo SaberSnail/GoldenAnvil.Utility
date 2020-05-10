f2df98c513610fd2089b2f2e5caf68c5b9c1ba4e
SaberSnail <andrew@batishko.net>
2020-05-10 07:53:34 -0700

    Add AsReadOnlyList; Remove obsolete method

--- GoldenAnvil.Utility/EnumerableUtility.cs
+++ GoldenAnvil.Utility/EnumerableUtility.cs
@@ -1,17 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace GoldenAnvil.Utility
 {
 	public static class EnumerableUtility
 	{
-		public static IEnumerable<T> Append<T>(this IEnumerable<T> items, T value)
-		{
-			foreach (T item in items)
-				yield return item;
-			yield return value;
-		}
-
 		public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> items)
 		{
 			return items ?? Enumerable.Empty<T>();
@@ -25,6 +20,24 @@ namespace GoldenAnvil.Utility
 		public static IEnumerable<T> Enumerate<T>(params T[] items)
 		{
 			return items;
+		}
+
+		public static IReadOnlyList<T> AsReadOnlyList<T>(this IEnumerable<T> items)
+		{
+			return items as IReadOnlyList<T> ??
+				(items is IList<T> list ? (IReadOnlyList<T>) new ReadOnlyListAdapter<T>(list) : items.ToList().AsReadOnly());
+		}
+
+		private sealed class ReadOnlyListAdapter<T> : IReadOnlyList<T>
+		{
+			public ReadOnlyListAdapter(IList<T> list) => m_list = list ?? throw new ArgumentNullException(nameof(list));
+
+			public int Count => m_list.Count;
+			public T this[int index] => m_list[index];
+			public IEnumerator<T> GetEnumerator() => m_list.GetEnumerator();
+			IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable) m_list).GetEnumerator();
+
+			readonly IList<T> m_list;
 		}
 	}
 }
