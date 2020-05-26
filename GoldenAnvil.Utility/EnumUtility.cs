bc73c124c44a59e1194485f68efe6a12936899fd
SaberSnail <andrew@batishko.net>
2020-05-25 18:23:18 -0700

    Add EnumUtility.Values

---
+++ GoldenAnvil.Utility/EnumUtility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldenAnvil.Utility
+{
+	public static class EnumUtility
+	{
+		public static T[] Values<T>() where T : struct => (T[]) Enum.GetValues(typeof(T));
+	}
+}
