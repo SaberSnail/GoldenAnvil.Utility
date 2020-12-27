20aaca8860a102494e51c9c48bc34483ab7c2b26
SaberSnail <andrew@batishko.net>
2020-12-27 14:54:34 -0800

    Add DisposableUtility

---
+++ GoldenAnvil.Utility/DisposableUtility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GoldenAnvil.Utility
+{
+	public static class DisposableUtility
+	{
+		public static void Dispose<T>(ref T obj)
+			where T : IDisposable
+		{
+			obj?.Dispose();
+			obj = default(T);
+		}
+	}
+}
