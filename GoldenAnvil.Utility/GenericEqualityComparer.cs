989161c598014c32bf0bfb0d32f765d80f35bb35
SaberSnail <andrew@batishko.net>
2020-12-27 14:54:44 -0800

    Add GenericEqualityComparer

---
+++ GoldenAnvil.Utility/GenericEqualityComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GoldenAnvil.Utility
+{
+	public sealed class GenericEqualityComparer<T> : IEqualityComparer<T>
+	{
+		public GenericEqualityComparer(Func<T, T, bool> compareFunc, Func<T, int> hashCodeFunc)
+		{
+			m_compareFunc = compareFunc ?? throw new ArgumentNullException(nameof(compareFunc));
+			m_hashCodeFunc = hashCodeFunc ?? throw new ArgumentNullException(nameof(hashCodeFunc));
+		}
+
+		public bool Equals(T left, T right) => m_compareFunc(left, right);
+
+		public int GetHashCode(T obj) => m_hashCodeFunc(obj);
+
+		readonly Func<T, T, bool> m_compareFunc;
+		readonly Func<T, int> m_hashCodeFunc;
+	}
+}
