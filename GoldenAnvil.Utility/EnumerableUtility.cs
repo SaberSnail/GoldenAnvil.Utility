f52e47499ff9dfcd3d4e81455d95653801ac3827
SaberSnail <andrew@batishko.net>
2021-04-04 17:46:04 -0700

    Add FirstOrDefault

--- GoldenAnvil.Utility/EnumerableUtility.cs
+++ GoldenAnvil.Utility/EnumerableUtility.cs
@@ -7,25 +7,43 @@ namespace GoldenAnvil.Utility
 {
 	public static class EnumerableUtility
 	{
-		public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> items)
-		{
-			return items ?? Enumerable.Empty<T>();
-		}
+		public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> items) => items ?? Enumerable.Empty<T>();
 
-		public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T> items)
-		{
-			return items.Where(x => x != null);
-		}
+		public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T> items) => items.Where(x => x != null);
 
-		public static IEnumerable<T> Enumerate<T>(params T[] items)
-		{
-			return items;
-		}
+		public static IEnumerable<T> Enumerate<T>(params T[] items) => items;
 
 		public static IReadOnlyList<T> AsReadOnlyList<T>(this IEnumerable<T> items)
 		{
 			return items as IReadOnlyList<T> ??
 				(items is IList<T> list ? (IReadOnlyList<T>) new ReadOnlyListAdapter<T>(list) : items.ToList().AsReadOnly());
+		}
+
+		public static T FirstOrDefault<T>(this IEnumerable<T> items, T defaultValue)
+		{
+			if (items is null)
+				throw new ArgumentNullException(nameof(items));
+
+			foreach (var item in items)
+				return item;
+
+			return defaultValue;
+		}
+
+		public static T FirstOrDefault<T>(this IEnumerable<T> items, Func<T, bool> predicate, T defaultValue)
+		{
+			if (items is null)
+				throw new ArgumentNullException(nameof(items));
+			if (predicate is null)
+				throw new ArgumentNullException(nameof(predicate));
+
+			foreach (var item in items)
+			{
+				if (predicate(item))
+					return item;
+			}
+
+			return defaultValue;
 		}
 
 		private sealed class ReadOnlyListAdapter<T> : IReadOnlyList<T>
