3beecabc1779e1a368da6732c370cd58dec96569
SaberSnail <andrew@batishko.net>
2023-02-27 17:14:58 -0800

    Add DebugLogDestination

---
+++ GoldenAnvil.Utility/Logging/DebugLogDestination.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GoldenAnvil.Utility.Logging
+{
+	public sealed class DebugLogDestination : ILogDestination
+	{
+		public DebugLogDestination()
+		{
+		}
+
+		public void LogMessage(LogSeverity severity, string source, string message)
+		{
+			var formattedMessage = $"{SeverityToString(severity)} - {source} - {message}";
+			Debug.WriteLine(formattedMessage);
+		}
+
+		private static string SeverityToString(LogSeverity severity)
+		{
+			switch (severity)
+			{
+			case LogSeverity.Info:
+				return "INFO";
+			case LogSeverity.Warn:
+				return "WARN";
+			case LogSeverity.Error:
+				return "ERROR";
+			default:
+				throw new NotImplementedException($"{nameof(SeverityToString)} is not implemented for '{severity}'.");
+			}
+		}
+	}
+}
