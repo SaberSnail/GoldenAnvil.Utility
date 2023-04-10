6c3e53956e4139561756590c0c07ad1b2399b37c
SaberSnail <andrew@batishko.net>
2023-04-10 14:23:44 -0700

    Add TypeDataTemplateSelector and TypeStyleSelector

---
+++ GoldenAnvil.Utility.Windows/TypeDataTemplateSelector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace GoldenAnvil.Utility.Windows
+{
+	[ContentProperty("Templates")]
+	public sealed class TypeDataTemplateSelector : DataTemplateSelector
+	{
+		public TypeDataTemplateSelector()
+		{
+			Templates = new Collection<TypeDataTemplate>();
+		}
+
+		public Collection<TypeDataTemplate> Templates { get; }
+
+		public override DataTemplate SelectTemplate(object item, DependencyObject container)
+		{
+			if (item is null)
+				return null;
+
+			var itemType = item.GetType();
+			return Templates
+				.Where(x => itemType.IsSameOrSubclassOf(x.Type))
+				.Select(x => x.Template)
+				.FirstOrDefault();
+		}
+	}
+
+	[ContentProperty("Template")]
+	public sealed class TypeDataTemplate
+	{
+		public Type Type { get; set; }
+		public DataTemplate Template { get; set; }
+	}
+}
---
+++ GoldenAnvil.Utility.Windows/TypeStyleSelector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows;
+
+namespace GoldenAnvil.Utility.Windows
+{
+	[ContentProperty("Styles")]
+	public sealed class TypeStyleSelector : StyleSelector
+	{
+		public TypeStyleSelector()
+		{
+			Styles = new Collection<TypeStyle>();
+		}
+
+		public Collection<TypeStyle> Styles { get; }
+
+		public override Style SelectStyle(object item, DependencyObject container)
+		{
+			if (item is null)
+				return null;
+
+			var itemType = item.GetType();
+			return Styles
+				.Where(x => itemType.IsSameOrSubclassOf(x.Type))
+				.Select(x => x.Style)
+				.FirstOrDefault();
+		}
+	}
+
+	[ContentProperty("Style")]
+	public sealed class TypeStyle
+	{
+		public Type Type { get; set; }
+		public Style Style { get; set; }
+	}
+}
