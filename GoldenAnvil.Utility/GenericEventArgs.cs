beacaec51ddd5bce0eff80a7fdf3cd5f274a60ed
SaberSnail <andrew@batishko.net>
2023-08-08 09:33:51 -0700

    Add GenericEventArgs

---
+++ GoldenAnvil.Utility/GenericEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GoldenAnvil.Utility
+{
+	public sealed class GenericEventArgs<T> : EventArgs
+	{
+		public GenericEventArgs(T value)
+		{
+			Value = value;
+		}
+
+		public T Value { get; }
+	}
+}
