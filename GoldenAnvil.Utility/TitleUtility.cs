302a1b05481d7e0e324ef97d651bd959481bee6e
SaberSnail <andrew@batishko.net>
2023-08-10 11:46:23 -0700

    Add TitleUtility

---
+++ GoldenAnvil.Utility/TitleUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace GoldenAnvil.Utility
+{
+	public static class TitleUtility
+	{
+		public static string GetUniqueTitle(string title, HashSet<string> existingTitles)
+		{
+			if (existingTitles == null)
+				throw new ArgumentNullException(nameof(existingTitles));
+
+			if (!existingTitles.Contains(title))
+				return title;
+
+			var baseTitleRegEx = new Regex("(.*) \\((\\d+)\\)$");
+			var result = baseTitleRegEx.Match(title);
+			var baseTitle = result.Success ? result.Groups[1].Value : title;
+			string bestTitle;
+			int index = result.Success ? StringUtility.TryParse<int>(result.Groups[2].Value) ?? 1 : 1;
+			do
+			{
+				bestTitle = $"{baseTitle} ({index})";
+				index++;
+			}
+			while (existingTitles.Contains(bestTitle));
+
+			return bestTitle;
+		}
+	}
+}
