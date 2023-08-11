fdf5b9d94cd606334971345925beeffb2d644f71
SaberSnail <andrew@batishko.net>
2023-08-11 08:30:48 -0700

    Add CustomTextBox

---
+++ GoldenAnvil.Utility.Windows/Controls/CustomTextBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GoldenAnvil.Utility.Windows.Controls
+{
+	public class CustomTextBox : TextBox
+	{
+		static CustomTextBox()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomTextBox), new FrameworkPropertyMetadata(typeof(CustomTextBox)));
+		}
+
+		public static readonly DependencyProperty HintProperty = DependencyPropertyUtility<CustomTextBox>.Register(x => x.Hint);
+
+		public object Hint
+		{
+			get { return GetValue(HintProperty); }
+			set { SetValue(HintProperty, value); }
+		}
+	}
+}
