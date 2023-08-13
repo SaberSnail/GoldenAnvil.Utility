6c5a703afad6cb56f57fa08bd78895db45176b5c
SaberSnail <andrew@batishko.net>
2023-08-13 09:37:20 -0700

    Add generic Convert method

---
+++ GoldenAnvil.Utility/ConversionUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GoldenAnvil.Utility
+{
+	public static class ConversionUtility
+	{
+		public static T Convert<T>(object value)
+		{
+			var t = typeof(T);
+			if (t.IsGenericType && t.GetGenericTypeDefinition() == typeof(Nullable<>))
+			{
+				if (value is null)
+					return default(T);
+
+				t = Nullable.GetUnderlyingType(t);
+			}
+			return (T) System.Convert.ChangeType(value, t);
+		}
+	}
+}
