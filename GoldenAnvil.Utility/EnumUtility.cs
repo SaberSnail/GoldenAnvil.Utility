92e1d49be256aaf5f4f33c29b5e7e1a9c3404ea2
SaberSnail <andrew@batishko.net>
2024-01-21 18:11:55 -0800

    Add EnumUtility.ToEnum

--- GoldenAnvil.Utility/EnumUtility.cs
+++ GoldenAnvil.Utility/EnumUtility.cs
@@ -4,6 +4,13 @@ namespace GoldenAnvil.Utility
 {
 	public static class EnumUtility
 	{
-		public static T[] Values<T>() where T : struct => (T[]) Enum.GetValues(typeof(T));
+		public static T[] Values<T>() where T : Enum => (T[]) Enum.GetValues(typeof(T));
+
+		public static T ToEnum<T>(int value) where T : Enum
+		{
+			if (!Enum.IsDefined(typeof(T), value))
+				throw new ArgumentException("Invalid enum value");
+			return (T)Enum.ToObject(typeof(T), value);
+		}
 	}
 }
