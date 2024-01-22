cdfcd0e6c1faf6b620fe386034ed8e95cbc1426c
SaberSnail <andrew@batishko.net>
2024-01-21 18:12:57 -0800

    Add SelectAfterFocusBehavior

---
+++ GoldenAnvil.Utility.Windows/Behaviors/SelectAfterFocusBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace GoldenAnvil.Utility.Windows.Behaviors
+{
+	public sealed class SelectAfterFocusBehavior : Behavior<TextBox>
+	{
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+			AssociatedObject.GotFocus += OnFocus;
+		}
+
+		protected override void OnDetaching()
+		{
+			base.OnDetaching();
+			AssociatedObject.GotFocus -= OnFocus;
+		}
+
+		private void OnFocus(object sender, RoutedEventArgs e)
+		{
+			AssociatedObject.SelectAll();
+		}
+	}
+}
