e5e838ad8e233953f96a2c551bc7c831f4e8d5a4
SaberSnail <SaberSnail@users.noreply.github.com>
2024-11-01 11:50:35 -0700

    Allow FocusAfterEventBehavior to work correctly when DataContext isn't initially set.

--- GoldenAnvil.Utility.Windows/Behaviors/FocusAfterEventBehavior.cs
+++ GoldenAnvil.Utility.Windows/Behaviors/FocusAfterEventBehavior.cs
@@ -20,22 +20,46 @@ namespace GoldenAnvil.Utility.Windows.Behaviors
 		{
 			base.OnAttached();
 
+			AssociatedObject.DataContextChanged += AssociatedObject_DataContextChanged;
+
 			var viewModel = AssociatedObject.DataContext;
-			var eventInfo = viewModel.GetType().GetEvent(EventName);
-			var methodInfo = GetType().GetMethod(nameof(OnEvent), BindingFlags.NonPublic | BindingFlags.Instance);
-			var handler = Delegate.CreateDelegate(eventInfo.EventHandlerType, this, methodInfo);
-			eventInfo.AddEventHandler(viewModel, handler);
+			if (viewModel is not null)
+				AddEventHandler(viewModel);
 		}
 
 		protected override void OnDetaching()
 		{
 			base.OnDetaching();
 
+			AssociatedObject.DataContextChanged -= AssociatedObject_DataContextChanged;
+
 			var viewModel = AssociatedObject.DataContext;
+			if (viewModel is not null)
+				RemoveEventHandler(viewModel);
+		}
+
+		private void AddEventHandler(object viewModel)
+		{
+			var eventInfo = viewModel.GetType().GetEvent(EventName);
+			var methodInfo = GetType().GetMethod(nameof(OnEvent), BindingFlags.NonPublic | BindingFlags.Instance);
+			var handler = Delegate.CreateDelegate(eventInfo.EventHandlerType, this, methodInfo);
+			eventInfo.AddEventHandler(viewModel, handler);
+		}
+
+		private void RemoveEventHandler(object viewModel)
+		{
 			var eventInfo = viewModel.GetType().GetEvent(EventName);
 			var methodInfo = GetType().GetMethod(nameof(OnEvent), BindingFlags.NonPublic | BindingFlags.Instance);
 			var handler = Delegate.CreateDelegate(eventInfo.EventHandlerType, this, methodInfo);
 			eventInfo.RemoveEventHandler(viewModel, handler);
+		}
+
+		private void AssociatedObject_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			if (e.OldValue is not null)
+				RemoveEventHandler(e.OldValue);
+			if (e.NewValue is not null)
+				AddEventHandler(e.NewValue);
 		}
 
 		private void OnEvent(object sender, EventArgs e)
