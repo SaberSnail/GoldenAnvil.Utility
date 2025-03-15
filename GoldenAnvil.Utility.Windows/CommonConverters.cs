a2883a9801b610004a70068a14a06987576a0be1
SaberSnail <SaberSnail@users.noreply.github.com>
2025-03-15 08:51:37 -0700

    Add visibility converter support for hidden

--- GoldenAnvil.Utility.Windows/CommonConverters.cs
+++ GoldenAnvil.Utility.Windows/CommonConverters.cs
@@ -11,15 +11,21 @@ namespace GoldenAnvil.Utility.Windows
 	{
 		public static readonly IValueConverter BooleanNot = new BooleanNotConverter();
 
-		public static readonly IValueConverter BooleanToVisibility = new BooleanToVisibilityConverter();
+		public static readonly IValueConverter BooleanToVisibility = new BooleanToVisibilityConverter(Visibility.Collapsed);
 
-		public static readonly IValueConverter BooleanNotToVisibility = new BooleanNotToVisibilityConverter();
+		public static readonly IValueConverter BooleanNotToVisibility = new BooleanNotToVisibilityConverter(Visibility.Collapsed);
+
+		public static readonly IValueConverter BooleanToVisibilityWithHidden = new BooleanToVisibilityConverter(Visibility.Hidden);
+
+		public static readonly IValueConverter BooleanNotToVisibilityWithHidden = new BooleanNotToVisibilityConverter(Visibility.Hidden);
 
 		public static readonly IValueConverter IsEqual = new IsEqualConverter();
 
 		public static readonly IValueConverter IsEqualToVisibility = new IsEqualToVisibilityConverter();
 
 		public static readonly IValueConverter IsNullToVisibility = new IsNullToVisibilityConverter();
+
+		public static readonly IValueConverter IsNullOrEmptyToVisibility = new IsNullOrEmptyToVisibilityConverter();
 
 		public static readonly IValueConverter IsNullOrEmptyToInverseVisibility = new IsNullOrEmptyToInverseVisibilityConverter();
 
@@ -45,34 +51,48 @@ namespace GoldenAnvil.Utility.Windows
 
 		private sealed class BooleanToVisibilityConverter : IValueConverter
 		{
+			public BooleanToVisibilityConverter(Visibility visibilityOnFalse)
+			{
+				m_visibilityOnFalse = visibilityOnFalse;
+			}
+
 			public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 			{
 				if (!targetType.IsAssignableFrom(typeof(Visibility)))
 					throw new InvalidOperationException(@"The target must be assignable from a Visibility.");
 
-				return (bool) value ? Visibility.Visible : Visibility.Collapsed;
+				return (bool) value ? Visibility.Visible : m_visibilityOnFalse;
 			}
 
 			public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
 			{
 				throw new NotImplementedException();
 			}
+
+			private Visibility m_visibilityOnFalse;
 		}
 
 		private sealed class BooleanNotToVisibilityConverter : IValueConverter
 		{
+			public BooleanNotToVisibilityConverter(Visibility visibilityOnTrue)
+			{
+				m_visibilityOnTrue = visibilityOnTrue;
+			}
+
 			public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 			{
 				if (!targetType.IsAssignableFrom(typeof(Visibility)))
 					throw new InvalidOperationException(@"The target must be assignable from a Visibility.");
 
-				return (bool) value ? Visibility.Collapsed : Visibility.Visible;
+				return (bool) value ? m_visibilityOnTrue : Visibility.Visible;
 			}
 
 			public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
 			{
 				throw new NotImplementedException();
 			}
+
+			private Visibility m_visibilityOnTrue;
 		}
 
 		private sealed class IsEqualConverter : IValueConverter
@@ -123,6 +143,28 @@ namespace GoldenAnvil.Utility.Windows
 			}
 		}
 
+		private sealed class IsNullOrEmptyToVisibilityConverter : IValueConverter
+		{
+			public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+			{
+				if (!targetType.IsAssignableFrom(typeof(Visibility)))
+					throw new InvalidOperationException(@"The target must be assignable from a Visibility.");
+
+				if (value is null)
+					return Visibility.Collapsed;
+				if (value is string stringValue)
+					return stringValue.Length == 0 ? Visibility.Collapsed : Visibility.Visible;
+				if (value is IEnumerable enumerable)
+					return enumerable.Cast<object>().Any() ? Visibility.Visible : Visibility.Collapsed;
+				return Visibility.Visible;
+			}
+
+			public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			{
+				throw new NotImplementedException();
+			}
+		}
+
 		private sealed class IsNullOrEmptyToInverseVisibilityConverter : IValueConverter
 		{
 			public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -134,7 +176,9 @@ namespace GoldenAnvil.Utility.Windows
 					return Visibility.Visible;
 				if (value is string stringValue)
 					return stringValue.Length == 0 ? Visibility.Visible : Visibility.Collapsed;
-				return Visibility.Visible;
+				if (value is IEnumerable enumerable)
+					return enumerable.Cast<object>().Any() ? Visibility.Collapsed : Visibility.Visible;
+				return Visibility.Collapsed;
 			}
 
 			public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
