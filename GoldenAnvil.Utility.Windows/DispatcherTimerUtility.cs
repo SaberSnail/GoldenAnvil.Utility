b3a5e6cf2157d828e430c449f5c728da379e3e9b
SaberSnail <SaberSnail@users.noreply.github.com>
2025-03-15 08:50:41 -0700

    Add DispatcherTimer.Restart

---
+++ GoldenAnvil.Utility.Windows/DispatcherTimerUtility.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Threading;
+
+namespace GoldenAnvil.Utility.Windows;
+
+public static class DispatcherTimerUtility
+{
+	public static void Restart(this DispatcherTimer timer)
+	{
+		timer.Stop();
+		timer.Start();
+	}
+}
