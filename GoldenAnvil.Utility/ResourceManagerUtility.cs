fad1cc9ce63f229583f726bbcb3385dd99b1fbee
SaberSnail <SaberSnail@users.noreply.github.com>
2025-04-13 08:40:05 -0700

    Clean up EnumToDisplayString

--- GoldenAnvil.Utility/ResourceManagerUtility.cs
+++ GoldenAnvil.Utility/ResourceManagerUtility.cs
@@ -5,11 +5,8 @@ namespace GoldenAnvil.Utility
 {
 	public static class ResourceManagerUtility
 	{
-		public static string EnumToDisplayString<T>(this ResourceManager resources, T enumValue) where T : struct
+		public static string EnumToDisplayString<T>(this ResourceManager resources, T enumValue) where T : Enum
 		{
-			if (!typeof(T).IsEnum)
-				throw new ArgumentException(@"T must be an enumerated type.");
-
 			var key = $"{typeof(T).Name}_{enumValue.ToString()}";
 			return resources.GetString(key) ?? enumValue.ToString();
 		}
