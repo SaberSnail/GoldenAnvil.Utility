0fbffd935c1e642f56f8a0dc09818b286bf15e4e
SaberSnail <SaberSnail@users.noreply.github.com>
2026-05-09 12:21:22 -0700

    Add EnumToDisplayString with modifiers

--- GoldenAnvil.Utility/ResourceManagerUtility.cs
+++ GoldenAnvil.Utility/ResourceManagerUtility.cs
@@ -10,5 +10,11 @@ namespace GoldenAnvil.Utility
 			var key = $"{typeof(T).Name}_{enumValue.ToString()}";
 			return resources.GetString(key) ?? enumValue.ToString();
 		}
+
+		public static string EnumToDisplayString<T>(this ResourceManager resources, T enumValue, string modifier) where T : Enum
+		{
+			var key = $"{typeof(T).Name}_{enumValue.ToString()}_{modifier}";
+			return resources.GetString(key) ?? enumValue.ToString();
+		}
 	}
 }
